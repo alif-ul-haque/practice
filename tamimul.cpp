e45702827a83e191867f8e50f5d1c97a6e379e63
alif-ul-haque <alifulhaque19@gmail.com>
2024-12-24 14:56:55 +0600

    new cpp file created

---
+++ tamimul.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define granite_state ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+#define line cout << "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+#define vsb (v).begin(),(v).end()
+#define vse (v).rbegin(),(v).rend()
+#define spc " "
+#define pb push_back
+#define pf push_front;
+
+signed main()
+ {
+    granite_state
+    cout<<"Fokir Tamimul "<<endl;
+}
+
+    ////////////////////////////////////////////////////
+    //                                                 //
+    //                 A                               //
+    //                                                 //
+    //                 L                               //
+    //                                                 //
+    //                 I                               //
+    //                                                 //
+    //                 F                               //
+    /////////////////////////////////////////////////////
+//think simply, it's always easier than it seems at first
+    //PEN AND PAPER instead of ms paint
+    //keep at it even if it feels exhausting(we're not doing this for fun anymore)
+    //2 GHONTAR AGER EDITORIAL DHORBI NA
